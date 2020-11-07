45238a5de77e8277e66540b3d47d0a4008564b89
aspsnd <2546697613@qq.com>
2020-11-07 14:18:00 +0800

    0

--- src/js/data/monst/all.js
+++ src/js/data/monst/all.js
@@ -2,6 +2,7 @@ const _MonstProtos = [];
 let files = require.context('./data', false, /\.js$/);
 files.keys().forEach(key => {
     let data = files(key).default;
+    if (!data) return;
     _MonstProtos[data.index] = data;
 })
 export const MonstProtos = _MonstProtos;
---
+++ src/js/data/monst/data/10.js
@@ -0,0 +1,12 @@
+import { MonstProto } from "../../../anxi/proto/monst";
+
+/**
+ * 火星神阿瑞斯
+ * 嗜血长矛
+ * 
+ * 技能1 嗜血长矛
+ * 技能2 熔火世界 半血时触发，陨石砸向自己，获得技能嗜血长矛
+ */
+export default new MonstProto({
+    index: 10,
+})
